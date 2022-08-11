fixed plot z axis scaling

neutron_bomb_1d.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import simpson
 import pandas as pd
+import math
 
 L = Config.critical_length_1d
 x_domain = np.linspace(0, L, Config.number_of_data_points)
@@ -36,6 +37,7 @@ def neutron_distribution(ap, x, t):
     Function which gives the neutron distribution at location x and time t
 
     Args:
+        ap (list) : list of fourier coefficients
         x (float) : location in the admitted domain <- (0,L)
         t (float) : time
     Returns:
@@ -68,8 +70,13 @@ def main():
         for idx2, time_loc in enumerate(time_domain):
             values[idx1, idx2] = neutron_distribution(ap, x_loc, time_loc)
 
+    
+    magnitude = int(math.log10(np.max(values))) # The magnitude is needed for label formatting
     axis_1, axis_2 = np.meshgrid(x_domain, time_domain)
     fig = plt.figure()
     ax = plt.axes(projection="3d")
-    ax.plot_surface(axis_1, axis_2, values, cstride=1, cmap="hot")
+    ax.plot_surface(axis_1, axis_2, values.T/(10**magnitude), cstride=1, cmap="hot") # THe division is necessary for the formatting of the figure
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Time axis')
+    ax.set_zlabel(r'Neutron density $(10^{' +rf'{magnitude}'+r'} m^{-3})$')
     plt.savefig("output\\1d_neutron_bomb\\1d_figure.png")

neutron_bomb_3d_cartesian.py
@@ -97,15 +97,17 @@ def main():
                     )
                 # Integration over x
                 apqr[i, j, k] = simpson(np.array(_integrated_y_strips), x_bar_domain)
-                flat_apqr.append([i, j, k, apqr[i, j, k]])
+                flat_apqr.append([i, j, k, apqr[i, j, k]]) 
+                # The flat apqr is necessary for comparison with the paper results
 
     dataframe = pd.DataFrame(data=flat_apqr)
     dataframe.to_csv("output\\3d_neutron_bomb_cartesian\\coefficients.csv")
 
     values = np.zeros((x_domain.size, y_domain.size, z_domain.size, time_domain.size))
+    # We only plot the neutron distribution at the last step of the time domain 
     for t_idx, t in enumerate(time_domain[-2:-1]):
         print(
-            "Computing the neutron density for the 3d cartesian neutrom bomb problem (4 progress bars)"
+            "Computing the neutron density for the 3d cartesian neutrom bomb problem"
         )
         for x_idx, x_loc in enumerate(tqdm(x_domain)):
             for y_idx, y_loc in enumerate(y_domain):
@@ -117,5 +119,10 @@ def main():
     axis_1, axis_2 = np.meshgrid(x_domain, y_domain)
     fig = plt.figure()
     ax = plt.axes(projection="3d")
+    # The neutron distribution is plotted only for z = L/2
     ax.plot_surface(axis_1, axis_2, values[:, :, int(N / 2), 0], cstride=1, cmap="hot")
+    ax.set_title('Neutron distribution at z = L/2')
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('y axis')
+    ax.set_zlabel(r'Neutron density  $( m^{-3})$')
     plt.savefig("output\\3d_neutron_bomb_cartesian\\3d_figure.png")

neutron_bomb_3d_cylindrical.py
@@ -7,6 +7,7 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 from scipy.special import jn_zeros, jv
 import pandas as pd
 from tqdm import tqdm
+import math
 
 
 L = Config.critical_length_cylindrical
@@ -70,14 +71,20 @@ def main():
     dataframe.to_csv("output\\3d_neutron_bomb_cylindrical\\coefficients.csv")
 
     print("Computing the neutron density for the cylindrical symmetry scenario")
+    # We only plot the neutron distribution at the last step of the time domain 
     values = np.zeros((r_domain.size, z_domain.size, time_domain.size))
     for t_idx, t in enumerate(time_domain[-2:-1]):
         for r_idx, r_loc in enumerate(tqdm(r_domain)):
             for z_idx, z_loc in enumerate(z_domain):
                 values[r_idx, z_idx, t_idx] = neutron_distribution(aq, r_loc, z_loc, t)
 
-    axis_1, axis_2 = np.meshgrid(r_domain, time_domain)
+    magnitude = int(math.log10(np.max(values))) # The magnitude is needed for label formatting
+    axis_1, axis_2 = np.meshgrid(r_domain, z_domain)
     fig = plt.figure()
     ax = plt.axes(projection="3d")
-    ax.plot_surface(axis_1, axis_2, values[:, :, 0], cstride=1, cmap="hot")
+    ax.plot_surface(axis_1, axis_2, values[:, :, 0]/(10**magnitude), cstride=1, cmap="hot")
+    ax.set_title('Neutron distribution at t=1e-5')
+    ax.set_xlabel('r axis')
+    ax.set_ylabel('z axis')
+    ax.set_zlabel(r'Neutron density $(10^{' +rf'{magnitude}'+r'} m^{-3})$')
     plt.savefig("output\\3d_neutron_bomb_cylindrical\\cylindrical_symmetry_figure.png")

neutron_bomb_3d_spherical.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import simpson
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import pandas as pd
-
+import math
 
 r1 = Config.critical_radius_spherical
 r_domain = np.linspace(0, r1, Config.number_of_data_points)
@@ -43,6 +43,7 @@ def neutron_distribution(ap, r, t):
     Function which gives the neutron distribution
 
     Args:
+        ap (list) : list of fourier coefficients
         r (float) : radial distance
         t (float) : time
     Returns:
@@ -85,8 +86,15 @@ def main():
         for idx2, j in enumerate(time_domain):
             values[idx1, idx2] = neutron_distribution(ap, i, j)
 
+
+    magnitude = int(math.log10(np.max(values))) # The magnitude is needed for label formatting
     axis_1, axis_2 = np.meshgrid(r_domain, time_domain)
     fig = plt.figure()
     ax = plt.axes(projection="3d")
-    ax.plot_surface(axis_1, axis_2, values, cstride=1, cmap="hot")
+    ax.plot_surface(axis_1, axis_2, values.T/(10**magnitude), cstride=1, cmap="hot")
+    ax.set_xlabel('r axis')
+    ax.set_ylabel('t axis')
+    ax.set_zlabel(r'Neutron density $(10^{' +rf'{magnitude}'+r'} m^{-3})$')
     plt.savefig("output\\3d_neutron_bomb_spherical\\spherical_symmetry_figure.png")
+
+main()

neutron_bomb_neumann.py
@@ -6,7 +6,7 @@ from scipy.integrate import simpson
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import pandas as pd
 from scipy.optimize import root_scalar
-
+import math
 
 R = Config.critical_radius_neumann
 r_domain = np.linspace(1e-6, R, Config.number_of_data_points)
@@ -15,7 +15,7 @@ time_domain = np.linspace(0, Config.time_limit_neumann, Config.number_of_data_po
 
 def _critical_radius(r):
     """
-    This equation computes the critical radius for sphere smaller than which, no runaway reaction can occur
+    The root of this equation gives the critical radius for sphere smaller than which, no runaway reaction can occur
     """
     _factor = np.sqrt(Config.eta / Config.mu)
     return (
@@ -25,7 +25,7 @@ def _critical_radius(r):
 
 def k_equation(k):
     """
-    This equation computes the k factor for an operational radius R = 8.5 cm
+    The root of this equation gives the k factor for an operational radius R = 8.5 cm
     """
     _beta = (1 / R) * (1 - (3 * R) / (2 * Config._lambda_t))
     return (1 / np.tan(k * R)) - (1 / k) * _beta
@@ -35,7 +35,7 @@ def neutron_distribution(r, t):
 
     K = root_scalar(k_equation, bracket=[0.5, np.pi / R]).root
     alpha = Config.mu * (K**2) - Config.eta
-    # Impose the n(R,t=0) = 1
+    # Impose the n(R,t=0) = 1 boundary condition
     A = R / np.sin(K * R)
 
     return A * np.exp(-alpha * t) * (np.sin(K * r) / r)
@@ -76,8 +76,12 @@ def main():
         for idx2, j in enumerate(time_domain):
             values[idx1, idx2] = neutron_distribution(i, j)
 
+    magnitude = int(math.log10(np.max(values))) # The magnitude is needed for label formatting
     axis_1, axis_2 = np.meshgrid(r_domain, time_domain)
     fig = plt.figure()
     ax = plt.axes(projection="3d")
-    ax.plot_surface(axis_1, axis_2, values, cstride=1, cmap="hot")
+    ax.plot_surface(axis_1, axis_2, values.T/(10**magnitude), cstride=1, cmap="hot")
+    ax.set_xlabel('r axis')
+    ax.set_ylabel('t axis')
+    ax.set_zlabel(r'Neutron density $(10^{' +rf'{magnitude}'+r'} m^{-3})$')
     plt.savefig("output\\3d_neutron_bomb_neuman\\neumann_figure.png")